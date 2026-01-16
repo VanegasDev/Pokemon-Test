d8916c781aa0066f571560f8fee0218818ddaf02
Mario Vanegas <mvanegasdev@gmail.com>
2026-01-15 22:52:28 -0600

    Initial Commit

---
+++ Pokemon-Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokemon-Test
+//
+//  Created by Mario Vanegas on 15/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pokemon-Test/Pokemon_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_TestApp.swift
+//  Pokemon-Test
+//
+//  Created by Mario Vanegas on 15/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
