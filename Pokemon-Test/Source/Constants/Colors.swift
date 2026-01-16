cacc37ef0ea5f60d2ea151c833102bf3c5cfdeec
Mario Vanegas <mvanegasdev@gmail.com>
2026-01-16 02:42:55 -0600

    🍱 - Adds Color Assets

---
+++ Pokemon-Test/Source/Constants/Colors.swift
@@ -0,0 +1,23 @@
+//
+//  Colors.swift
+//  Pokemon-Test
+//
+//  Created by Mario Vanegas on 14/1/26.
+//
+
+extension Constants {
+    enum Colors {
+        static let black = "Black"
+        static let blue = "Blue"
+        static let green = "Green"
+        static let indigo = "Indigo"
+        static let lightBlue = "LightBlue"
+        static let lightGray = "LightGray"
+        static let lightPink = "LightPink"
+        static let lightYellow = "LightYellow"
+        static let pink = "Pink"
+        static let purple = "Purple"
+        static let white = "White"
+        static let yellow = "Yellow"
+    }
+}
--- Pokemon-Test/Source/Constants/Icons.swift
+++ Pokemon-Test/Source/Constants/Icons.swift
@@ -9,14 +9,16 @@ extension Constants {
     // I want static let for this icon names
     // Electric, Fire, Grass, Height, Weight, Normal, Poison, Pokemon, Search, Water
     
-    static let electric = "Electric"
-    static let fire = "Fire"
-    static let grass = "Grass"
-    static let height = "Height"
-    static let weight = "Weight"
-    static let normal = "Normal"
-    static let poison = "Poison"
-    static let pokemon = "Pokemon"
-    static let search = "Search"
-    static let water = "Water"
+    enum Icons {
+        static let electric = "Electric"
+        static let fire = "Fire"
+        static let grass = "Grass"
+        static let height = "Height"
+        static let weight = "Weight"
+        static let normal = "Normal"
+        static let poison = "Poison"
+        static let pokemon = "Pokemon"
+        static let search = "Search"
+        static let water = "Water"
+    }
 }
