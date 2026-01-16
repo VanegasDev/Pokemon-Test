4201be13605a7aee2c61a14c7729b37a5bf8bb3c
Mario Vanegas <mvanegasdev@gmail.com>
2026-01-16 04:21:25 -0600

    🚧 - Adds Pokemon Home View

---
+++ Pokemon-Test/Source/ViewModels/HomeViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  HomeViewModel.swift
+//  Pokemon-Test
+//
+//  Created by Mario Vanegas on 14/1/26.
+//
+
--- Pokemon-Test/SupportingFiles/Pokemon_TestApp.swift
+++ Pokemon-Test/SupportingFiles/Pokemon_TestApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Pokemon_TestApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
